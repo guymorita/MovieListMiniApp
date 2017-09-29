faec74e74e20193af21d82fec7cbdd3970657e33
Guy Morita - vn0om6v <GMorita@walmart.com>
2017-09-29 11:37:14 -0400

    Update MovieList MiniApp UI

--- index.ios.js
+++ index.ios.js
@@ -1,32 +1,67 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
+  View,
+  ListView,
+  Image,
+  TouchableHighlight
+} from "react-native";
 
 export default class MovieListMiniApp extends Component {
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+    this.state = {
+      dataSource: ds.cloneWithRows([
+        {
+          title: "The Fast and Furious",
+          releaseYear: 2010,
+          ratings: "4.5",
+          imageUrl: "http://bit.ly/2jRUZwE",
+          description: "The Fast and the Furious"
+        },
+        {
+          title: "2 Fast 2 Furious",
+          releaseYear: 2011,
+          ratings: "4.0",
+          imageUrl: "http://bit.ly/2jTfYPF",
+          description: "How fast do you like it ?"
+        }
+      ])
+    };
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <ListView
+        style={styles.container}
+        dataSource={this.state.dataSource}
+        renderRow={movie => (
+          <View style={styles.row}>
+            <Image
+              style={styles.icon}
+              source={{
+                uri: movie.imageUrl ? movie.imageUrl : "http://bit.ly/2yz3AYe"
+              }}
+            />
+            <View style={styles.row2}>
+              <Text style={styles.title}>{movie.title}</Text>
+              <Text style={styles.subtitle}>{movie.releaseYear}</Text>
+            </View>
+          </View>
+        )}
+        renderSeparator={(sectionId, rowId) => (
+          <View key={rowId} style={styles.separator} />
+        )}
+      />
     );
   }
 }
@@ -34,20 +69,51 @@ export default class MovieListMiniApp extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    marginTop: 20,
+    padding: 5,
+    backgroundColor: "black"
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  row: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "white",
+    padding: 12
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  row2: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-between",
+    backgroundColor: "white",
+    padding: 12
   },
+  listview: {
+    flex: 1,
+    padding: 12,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  title: {
+    fontSize: 20
+  },
+  subtitle: {
+    paddingTop: 5,
+    flex: 1,
+    fontSize: 12
+  },
+  separator: {
+    flex: 1,
+    height: 0,
+    paddingTop: 2,
+    paddingBottom: 2
+  },
+  icon: {
+    width: 50,
+    height: 70,
+    flexShrink: 1,
+    alignSelf: "center"
+  }
 });
 
-AppRegistry.registerComponent('MovieListMiniApp', () => MovieListMiniApp);
+AppRegistry.registerComponent("MovieListMiniApp", () => MovieListMiniApp);
